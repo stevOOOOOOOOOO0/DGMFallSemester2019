98b5d50131a6380941eec656661e6d89bbc06c4c
514 Classroom <stevOOOOOOOOOO0@yahoo.com>
2019-08-21 14:37:58 -0600

    work 8/21/2019
    created a floatdata scriptable object

---
+++ Assets/Scripts/FloatData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu] 
+public class FloatData : ScriptableObject
+{
+
+	public float Value = 1f;
+
+	public void UpdateValue(float number)
+	{
+		Value += number;
+	}
+}
---
+++ Assets/Scripts/TriggerBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBehaviour : MonoBehaviour {
+
+	public FloatData data;
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		data.UpdateValue(-.1f);
+	}
+}
